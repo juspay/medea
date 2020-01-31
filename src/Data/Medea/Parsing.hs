f401ffc21ebfb698102b36b3d21e559f79057ed4
Koz Ross <koz.ross@retro-freedom.nz>
2020-02-01 10:44:59 +1300

    Redraft parser to do UTF-8 validation
    Streamly has a way to do this, as described in
    https://composewell.github.io/streamly/v0.7.0/Streamly-Internal-Data-Unicode-Stream.html#v:decodeUtf8Either
    - the parser now makes use of it.

--- src/Data/Medea/Parsing.hs
+++ src/Data/Medea/Parsing.hs
@@ -1,77 +1,49 @@
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Data.Medea.Parsing where
 
-import Data.Char (GeneralCategory(..), generalCategory)
+import Data.Text.Short (ShortText, pack)
 import Control.Monad.State.Strict (MonadState(..), modify)
 import Control.Monad.Except (MonadError(..))
 import Data.Word (Word8)
-import Streamly.Data.Unicode.Stream (decodeUtf8)
-import Streamly (SerialT, MonadAsync)
-import Data.Text.Short (ShortText, pack, isPrefixOf, toByteString)
+import Streamly.Internal.Data.Unicode.Stream (decodeUtf8Either, lines)
+import Streamly (IsStream, MonadAsync)
+import Prelude hiding (lines, words)
 
-import qualified Data.ByteString as BS
 import qualified Streamly.Prelude as SP
 import qualified Streamly.Data.Fold as SDF
 
-import Data.Medea.Identifier (Identifier(..))
-import Data.Medea.Primitive (Primitive(..))
-
-newtype ParserState = ParserState {
-  lineNumber :: Word
-}
-
-data ParserError = ParserError {
-  context :: Word,
-  err :: ParserErrorType
-}
-
-data ParserErrorType = 
+data ParseError = 
   -- got some non-UTF8 encoding
-  NotUTF8 |
-  -- someone fed us an identifier of excessive length
-  IdentifierTooLong Int | -- length we encountered
-  -- attempt to define a reserved identifier
-  ReservedName ShortText -- the string they tried to use
+  NotUTF8
 
 -- Parser innards
 
-data Token = 
-  SchemataMarker |
-  SchemaMarker |
-  TypeMarker |
-  PrimitiveTypeMarker Primitive |
-  InitialSchemaMarker |
-  SchemaName Identifier
-  deriving (Eq)
+data LineToken =
+  -- First '$schemata' line 
+  SchemataHeader |
+  -- Identifier in schemata section
+  SchemaDeclaration ShortText |
+  -- A '$schema' line starting a schema def
+  SchemaHeader ShortText |
+  -- $type - start of a type spec section
+  TypeSpecHeader |
+  -- A single type specifier
+  TypeSpecifier ShortText
 
-makeToken :: (MonadError ParserError m) => 
-  (Word, ShortText) -> m (Word, Token)
-makeToken (line, st)
-  | st == "$schemata" = pure (line, SchemataMarker)
-  | st == "$schema" = pure (line, SchemaMarker)
-  | st == "$type" = pure (line, TypeMarker)
-  | st == "$null" = pure (line, PrimitiveTypeMarker NullT)
-  | st == "$boolean" = pure (line, PrimitiveTypeMarker BooleanT)
-  | st == "$object" = pure (line, PrimitiveTypeMarker ObjectT)
-  | st == "$number" = pure (line, PrimitiveTypeMarker NumberT)
-  | st == "$string" = pure (line, PrimitiveTypeMarker StringT)
-  | st == "$array" = pure (line, PrimitiveTypeMarker ArrayT)
-  | st == "$start" = pure (line, InitialSchemaMarker)
-  | "$" `isPrefixOf` st = throwError . ParserError line . ReservedName $ st
-  | otherwise = do
-      let len = BS.length . toByteString $ st
-      if len <= 32
-      then pure (line, SchemaName . Identifier $ st)
-      else throwError . ParserError line . IdentifierTooLong $ len
+resolveUtf8Encoding :: (MonadAsync m, MonadError ParseError m, IsStream t) => 
+  t m Word8 -> t m Char
+resolveUtf8Encoding = SP.mapM go . decodeUtf8Either
+  where go = \case Left _ -> throwError NotUTF8
+                   Right c -> pure c
 
-tokenize :: (MonadAsync m, MonadError ParserError m, MonadState Word m) => 
-  SerialT m Word8 -> SerialT m (Word, Token)
-tokenize = SP.mapM makeToken . SP.wordsBy isNewline gatherString . decodeUtf8
-  where isNewline = (LineSeparator ==) . generalCategory
-        gatherString = SDF.mapM go . fmap pack $ SDF.toList
-        go st = do
+tagLines :: (MonadAsync m, IsStream t, MonadState Word m) => 
+  t m Char -> t m (Word, ShortText)
+tagLines = SP.mapM go . lines (pack <$> SDF.toList)
+  where go w = do
           line <- get
           modify (+ 1)
-          pure (line, st)
+          pure (line, w)
+

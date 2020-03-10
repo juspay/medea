a37286ddf3cb96695e4879ecd84de16d9e48e05d
shaurya gupta <shauryab98@gmail.com>
2020-03-10 18:51:06 +0530

    Refactor testing
    - Add fail/pass subdirectories in parser tests.
    - Add TestM.Util.
    - Refactor out listMedeaFiles

---
+++ test/TestM/Util.hs
@@ -0,0 +1,10 @@
+module TestM.Util
+  (listMedeaFiles)
+where
+
+import Data.List (sort)
+import System.Directory (listDirectory)
+import System.FilePath ((</>), isExtensionOf)
+
+listMedeaFiles :: FilePath -> IO [FilePath]
+listMedeaFiles dir = fmap (dir </>) . sort . filter (isExtensionOf ".medea") <$> listDirectory dir
